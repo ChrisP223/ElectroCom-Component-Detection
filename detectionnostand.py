47a4e80c9dd7fd3b743e332f38bc96cce8703500
ChrisP223 <christiannaap662@gmail.com>
2026-05-10 00:15:13 +0300

    Update detectionnostand.py

--- detectionnostand.py
+++ detectionnostand.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    results = model(frame,conf=0.12,verbose=False)
+    results = model(frame,conf=0.25,verbose=False)
     annotated = results[0].plot()
     cv2.imshow("YOLOv8", annotated)
     if cv2.waitKey(1) & 0xFF == ord('q'):
